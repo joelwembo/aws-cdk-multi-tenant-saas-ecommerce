4aa2605262e026a10c08a1cd351d7f5d80f3f1d1
Joel Wembo <joelotepawembo@gmail.com>
2023-09-30 19:36:44 +0800

    updated system

--- bin/cdk-ts-api-gateway.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { CdkTsApiGatewayStack } from '../lib/cdk-ts-api-gateway-stack';
-import environmentConfig from './stack-config';
-
-const app = new cdk.App();
-new CdkTsApiGatewayStack(app, 'CdkTsApiGatewayStack', environmentConfig);
