57c1ac5a9eafc507966bb9ffc751bf9171a9079c
Joel Wembo <joelotepawembo@gmail.com>
2023-10-04 23:32:55 +0800

    updated aws micro services sqs test

--- test/aws-microservices.test.ts
+++ test/aws-microservices.test.ts
@@ -1,17 +1,17 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsMicroservices from '../lib/aws-microservices-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import * as AwsCloudappStack from '../lib/aws-cloudapp-stack';
 
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/aws-microservices-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsMicroservices.AwsCloudappStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  const app = new cdk.App();
+    // WHEN
+  const stack = new AwsCloudappStack.AwsCloudappStack(app, 'MyTestStack');
+    // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  template.hasResourceProperties('AWS::SQS::Queue', {
+    VisibilityTimeout: 300
+  });
 });
